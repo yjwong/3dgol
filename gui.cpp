bcafa407e9b30bec93ea34d70777ddbe9a86f792
Wong Yong Jie <yjwong92@gmail.com>
2014-11-13 14:24:04 +0800

    Fixed indentation.

--- gui.cpp
+++ gui.cpp
@@ -12,51 +12,51 @@ static const int EXIT_INVALID_ARGS = 2;
 static const int EXIT_UNKNOWN_FAILURE = 3;
 
 int cstrToInt(char* data) {
-	std::istringstream ss(data);
-	int num;
-	if (!(ss >> num)) {
-		throw std::domain_error("Supplied argument is not an integer.");
-	}
+    std::istringstream ss(data);
+    int num;
+    if (!(ss >> num)) {
+        throw std::domain_error("Supplied argument is not an integer.");
+    }
 
-	return num;
+    return num;
 }
 
 int main(int argc, char* argv[]) {
-	// Check for arguments.
-	if (argc != 7 && argc != 8) {
-		std::cout << argv[0] << ": Insufficient arguments to the program." <<
-			std::endl;
-		std::cout << argv[0] << ": Required arguments are (in order): " <<
-			std::endl;
-		std::cout << argv[0] << ":     sz, r1, r2, r3, r4, sp" <<
-			std::endl;
-		std::cout << argv[0] << ": Optional arguments are: fl" << std::endl;
-		return EXIT_INSUFFICIENT_ARGS;
-	}
+    // Check for arguments.
+    if (argc != 7 && argc != 8) {
+        std::cout << argv[0] << ": Insufficient arguments to the program." <<
+            std::endl;
+        std::cout << argv[0] << ": Required arguments are (in order): " <<
+            std::endl;
+        std::cout << argv[0] << ":     sz, r1, r2, r3, r4, sp" <<
+            std::endl;
+        std::cout << argv[0] << ": Optional arguments are: fl" << std::endl;
+        return EXIT_INSUFFICIENT_ARGS;
+    }
 
-	// Initialize arguments.
-	int sz, r1, r2, r3, r4, sp;
-	std::string fl;
-	try {
-		sz = cstrToInt(argv[1]);
-		r1 = cstrToInt(argv[2]);
-		r2 = cstrToInt(argv[3]);
-		r3 = cstrToInt(argv[4]);
-		r4 = cstrToInt(argv[5]);
-		sp = cstrToInt(argv[6]);
-	} catch (std::domain_error &e) {
-		std::cout << argv[0] << ": All arguments except for fl must be " <<
-			"integers." << std::endl;
-		return EXIT_INVALID_ARGS;
-	}
+    // Initialize arguments.
+    int sz, r1, r2, r3, r4, sp;
+    std::string fl;
+    try {
+        sz = cstrToInt(argv[1]);
+        r1 = cstrToInt(argv[2]);
+        r2 = cstrToInt(argv[3]);
+        r3 = cstrToInt(argv[4]);
+        r4 = cstrToInt(argv[5]);
+        sp = cstrToInt(argv[6]);
+    } catch (std::domain_error &e) {
+        std::cout << argv[0] << ": All arguments except for fl must be " <<
+            "integers." << std::endl;
+        return EXIT_INVALID_ARGS;
+    }
 
-	// Parse the last optional argument.
-	if (argc == 8) {
-		fl.append(argv[7]);
-	}
+    // Parse the last optional argument.
+    if (argc == 8) {
+        fl.append(argv[7]);
+    }
 
-	// Initialize the Game of Life.
-	GameOfLife::GameOfLife program (sz, r1, r2, r3, r4, fl);
+    // Initialize the Game of Life.
+    GameOfLife::GameOfLife program (sz, r1, r2, r3, r4, fl);
     GameOfLife::Display display (&program, sp);
     return EXIT_OK;
 }
